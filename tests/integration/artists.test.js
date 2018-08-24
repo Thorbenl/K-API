28f4aa3e2f14b29ef3a3e71d2fe97b2bcbcafb51
Thorben L <turbzcoding@gmail.com>
2018-08-24 23:21:24 +0200

    Added a generic test for creating artists

--- tests/integration/artists.test.js
+++ tests/integration/artists.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const Artist = require('../../models/artist');
 let server;
 
 describe('/api/artists', () => {
@@ -6,14 +7,20 @@ describe('/api/artists', () => {
     beforeEach(() => {
         server = require('../../app');
     });
-    afterEach(() => {
+    afterEach(async () => {
         server.close();
+        await Artist.remove({});
     });
 
     describe('GET /artists', () => {
         it('should return all artists', async () => {
+            await Artist.collection.insertMany([
+                { stageName: 'artist1' },
+                { stageName: 'artist2' }
+            ]);
             const res = await request(server).get('/api/artists');
             expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
         });
     });
 });
