0e486dce537b6ff0c52c1c81ff71b3469f0f5055
Thorben L <turbzcoding@gmail.com>
2018-08-24 22:43:41 +0200

    Forgot to call the function for logging, also added the first real unit test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const winston = require('winston');
 const express = require('express');
 const app = express();
-require('./startup/logging');
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
